c53cea3a98098986cf247b4e8861e3c50dbd9b76
Guilherme <guiwerlichdev@gmail.com>
2024-04-23 10:42:09 -0300

    tarefa x finalizada

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+
+    if (player1 === "Pedra") {
+        if (player2 === "Tesoura") {
+            return "Jogador 1 venceu!"
+        } else if (player2 === "Papel") {
+            return "Jogador 2 venceu!"
+        } else if (player2 === "Pedra") {
+            return "Empate!"
+        }
+    } else if (player1 === "Papel") {
+        if (player2 === "Pedra") {
+            return "Jogador 1 venceu!"
+        } else if (player2 === "Tesoura") {
+            return "Jogador 2 venceu!"
+        } else if (player2 === "Papel") {
+            return "Empate!"
+        }
+    } else if (player1 === "Tesoura") {
+        if (player2 === "Papel") {
+            return "Jogador 1 venceu!"
+        } else if (player2 === "Pedra") {
+            return "Jogador 2 venceu!"
+        } else if (player2 === "Tesoura") {
+            return "Empate!"
+        }
+    } 
+}
+
+const resultado = playRockPaperScissor("Pedra", "Papel")
+console.log(resultado)
