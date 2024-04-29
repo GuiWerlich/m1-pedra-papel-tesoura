006378e4405f8475666b909db25dee65b5dd66f1
GuiWerlich <guiwerlichdev@gmail.com>
2024-04-28 21:03:04 -0300

    tarefa x finalizada

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
 function playRockPaperScissor(player1, player2) {
-
-    if (player1 === "Pedra") {
-        if (player2 === "Tesoura") {
-            return "Jogador 1 venceu!"
-        } else if (player2 === "Papel") {
-            return "Jogador 2 venceu!"
-        } else if (player2 === "Pedra") {
-            return "Empate!"
-        }
-    } else if (player1 === "Papel") {
-        if (player2 === "Pedra") {
-            return "Jogador 1 venceu!"
-        } else if (player2 === "Tesoura") {
-            return "Jogador 2 venceu!"
-        } else if (player2 === "Papel") {
-            return "Empate!"
-        }
-    } else if (player1 === "Tesoura") {
-        if (player2 === "Papel") {
-            return "Jogador 1 venceu!"
-        } else if (player2 === "Pedra") {
-            return "Jogador 2 venceu!"
-        } else if (player2 === "Tesoura") {
-            return "Empate!"
-        }
-    } 
+    if ((player1 === "Pedra" && player2 === "Tesoura") ||
+        (player1 === "Papel" && player2 === "Pedra") ||
+        (player1 === "Tesoura" && player2 === "Papel")) {
+        return "Jogador 1 venceu!";
+    } else if ((player1 === "Pedra" && player2 === "Papel") ||
+               (player1 === "Papel" && player2 === "Tesoura") ||
+               (player1 === "Tesoura" && player2 === "Pedra")) {
+        return "Jogador 2 venceu!";
+    } else if (player1 === player2) {
+        return "Empate!";
+    }
 }
 
-const resultado = playRockPaperScissor("Pedra", "Papel")
-console.log(resultado)
+const resultado = playRockPaperScissor("Pedra", "Papel");
+console.log(resultado);
